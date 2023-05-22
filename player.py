5a91cae189d8102a365fb76bb4508beef170609a
MimzUi32 <Reload3322@hotmail.com>
2023-05-22 04:44:00 +0300

    edit

--- player.py
+++ player.py
@@ -28,16 +28,16 @@ x_bottom = game_dimensions['x_bottom']
 y_top = game_dimensions['y_top']
 y_bot = game_dimensions['y_bot']
 
-left_region = (y_top, x_top+30, 150, y_bot-y_top)
-right_region = (x_bottom-150, x_top+30, 150, y_bot-y_top)
+left_region = (x_top, y_top, 150, y_bot-y_top)
+right_region = (x_bottom-150, y_top, 150, y_bot-y_top)
 bottom_region = (x_top, y_bot-155, x_bottom-x_top, 160)
-top_region = (x_top+150, x_top+30, x_bottom-x_top-150, 45)
+top_region = (x_top, y_top+30, x_bottom-x_top, 45)
+
 
 with open('player_image_paths.txt', 'r') as file:
     player_image_path = file.readline().strip()
     player_top_image_path = file.readline().strip()
-    print(player_image_path)
-    print(player_top_image_path)
+
 
 # Load the player image and convert it to grayscale
 player = cv2.imread(player_image_path, cv2.IMREAD_GRAYSCALE)
@@ -47,21 +47,25 @@ player_top = cv2.imread(player_top_image_path, cv2.IMREAD_GRAYSCALE)
 method = cv2.TM_CCOEFF_NORMED
 
 # Define a threshold for the similarity score
-threshold = 0.9
-#cv2.imshow('test',np.array(pyautogui.screenshot(region=bottom_region)))
-#cv2.waitKey()
+threshold = 0.82
+
+
+# Define a function for performing template matching in a given region
+
+def match_template(region, result, player_image):
+    # Take a screenshot of the region
+    screenshot = np.array(pyautogui.screenshot(region=region))
+    # Convert the screenshot to grayscale
+    gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+    # Perform template matching for the player image against the region
+    result[0] = (cv2.matchTemplate(gray, player_image, method), gray)  # return matchTemplate result and gray
+
+    
 while True:
     if not os.path.exists(temp_file_name):
         # The temp file has been deleted, stop the script
         sys.exit()
-    # Define a function for performing template matching in a given region
-    def match_template(region, result, player_image):
-        # Take a screenshot of the region
-        screenshot = np.array(pyautogui.screenshot(region=region))
-        # Convert the screenshot to grayscale
-        gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
-        # Perform template matching for the player image against the region
-        result[0] = cv2.matchTemplate(gray, player_image, method)
+
 
     # Define a list to hold the results of template matching for each region
     results = [None, None, None, None]
@@ -88,7 +92,12 @@ while True:
         thread.join()
 
     # Extract the maximum similarity score for each region from the results
-    max_vals = [result[0].max() for result in results]
+    max_vals = []
+    for result in results:
+        max_val, gray = result[0][0].max(), result[0][1]
+        max_vals.append(max_val)
+
+    print(max_vals)
 
     if max_vals[0] > threshold:
         play(song)
@@ -101,6 +110,6 @@ while True:
         play(song)
         sleep(0.2)
 
-    elif max_vals[3] > threshold:
+    elif max_vals[3] > 0.9:
         play(song)
         sleep(0.2)
